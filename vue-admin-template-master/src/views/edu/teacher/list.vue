ecf1bea5a7229821f3b3b20e294a37b1dbaf639e
Lc <641229781@qq.com>
2020-12-31 16:23:56 +0800

    讲师列表和分页完成

--- vue-admin-template-master/src/views/edu/teacher/list.vue
+++ vue-admin-template-master/src/views/edu/teacher/list.vue
@@ -1,6 +1,55 @@
 <template>
-  <div class="login-container">
-    讲师列表
+  <div>
+         <!-- 表格 -->
+    <el-table
+      :data="list"
+      element-loading-text="数据加载中"
+      border
+      fit
+      highlight-current-row>
+
+      <el-table-column
+        label="序号"
+        width="70"
+        align="center">
+        <template slot-scope="scope">
+          {{ (page - 1) * limit + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="name" label="名称" width="80" />
+
+      <el-table-column label="头衔" width="80">
+        <template slot-scope="scope">
+          {{ scope.row.level===1?'高级讲师':'首席讲师' }}
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="intro" label="资历" />
+
+      <el-table-column prop="gmtCreate" label="添加时间" width="160"/>
+
+      <el-table-column prop="sort" label="排序" width="60" />
+
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <router-link :to="'/edu/teacher/edit/'+scope.row.id">
+            <el-button type="primary" size="mini" icon="el-icon-edit">修改</el-button>
+          </router-link>
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeDataById(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+     <!-- 分页 -->
+    <el-pagination
+      :current-page="page"
+      :page-size="limit"
+      :total="total"
+      style="padding: 30px 0; text-align: center;"
+      layout="total, prev, pager, next, jumper"
+      @current-change="getList"
+    />
   </div>
 </template>
 
@@ -8,14 +57,31 @@
 import teacher from '@/api/edu/teacher/teacher'
 export default {  
     data() {
-
+        return {
+            page:1,
+            limit:3,
+            teacherQuery: {},
+            total: 0,
+            list:[]
+        }
     },
     created() {
-
+        this.getList();
     },
     methods: {
-        
+        getList(page = 1) {
+            this.page = page;
+            teacher.getTeacherListPage(this.page, this.limit,this.teacherQuery)
+                .then(res => {
+                    console.log(res)
+                    this.list = res.data.rows
+                    this.total = res.data.total
+                }).catch(err => {
+                    console.log(err)
+                })
+        }
     }
+    
 }
 </script>
 
